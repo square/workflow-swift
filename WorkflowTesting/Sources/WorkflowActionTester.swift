cd8320011468926ba1963d61d100dae47cbac9c7
Noah Blake <nononoah@gmail.com>
2020-08-17 12:13:03 -0400

    [Gardening] Correct typo in deprecation warning

--- WorkflowTesting/Sources/WorkflowActionTester.swift
+++ WorkflowTesting/Sources/WorkflowActionTester.swift
@@ -168,7 +168,7 @@ extension WorkflowActionTester {
         return self
     }
 
-    @available(*, deprecated, message: "use `send(action:)` followed by `verifyOutput(_:)`, `verify(outpit:)` or `assertNoOutput()`")
+    @available(*, deprecated, message: "use `send(action:)` followed by `verifyOutput(_:)`, `verify(output:)` or `assertNoOutput()`")
     @discardableResult
     public func send(action: Action, outputAssertions: (WorkflowType.Output?) -> Void = { _ in }) -> WorkflowActionTester<WorkflowType, Action> {
         return send(action: action)
