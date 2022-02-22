8cf57c48fccb2668d710e36ab18c9b7e9a8c7f4b
Mark Johnson <markj@squareup.com>
2022-02-22 10:51:37 -0500

    Added tests for ObservableListener

---
+++ WorkflowRxSwift/Tests/WorkflowHostObservableListenerTests.swift
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2022 Square Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import RxSwift
+import Workflow
+import WorkflowTesting
+import XCTest
+@testable import WorkflowRxSwift
+
+class WorkflowHostObservableListenerTests: XCTestCase {
+    func test_addingObservableRenderingListener() {
+        let host = WorkflowHost(workflow: TestWorkflow(step: .first))
+
+        let renderingsComplete = expectation(description: "Waiting for renderings")
+        let subscription = host.renderingObservable.subscribe(onNext: { rendering in
+            XCTAssertEqual(2, rendering)
+            renderingsComplete.fulfill()
+        })
+
+        host.update(workflow: TestWorkflow(step: .second))
+
+        waitForExpectations(timeout: 1)
+        subscription.dispose()
+    }
+
+    func test_addingObservableOutputListener() {
+        let host = WorkflowHost(workflow: TestOutputWorkflow(state: 0))
+
+        let outputComplete = expectation(description: "Waiting for output")
+        let subscription = host.outputObservable.subscribe(onNext: { output in
+            XCTAssertEqual(1, output)
+            outputComplete.fulfill()
+        })
+
+        host.rendering.onIncrement()
+
+        waitForExpectations(timeout: 1)
+        subscription.dispose()
+    }
+
+    fileprivate struct TestWorkflow: Workflow {
+        var step: Step
+        enum Step {
+            case first
+            case second
+        }
+
+        struct State {}
+        func makeInitialState() -> State {
+            return State()
+        }
+
+        typealias Rendering = Int
+
+        func render(state: State, context: RenderContext<TestWorkflow>) -> Rendering {
+            switch step {
+            case .first:
+                return 1
+            case .second:
+                return 2
+            }
+        }
+    }
+
+    fileprivate struct IncrementAction: WorkflowAction {
+        typealias WorkflowType = TestOutputWorkflow
+
+        let value: Int
+
+        func apply(toState state: inout Int) -> TestOutputWorkflow.Output? {
+            state += value
+            return state
+        }
+    }
+
+    fileprivate struct TestOutputRendering {
+        let onIncrement: () -> Void
+    }
+
+    fileprivate struct TestOutputWorkflow: Workflow {
+        func makeInitialState() -> State {
+            return 0
+        }
+
+        typealias Rendering = TestOutputRendering
+        typealias Output = Int
+        typealias State = Int
+
+        var state: State
+
+        func render(state: State, context: RenderContext<TestOutputWorkflow>) -> Rendering {
+            let sink = context.makeSink(of: IncrementAction.self)
+            return TestOutputRendering {
+                sink.send(IncrementAction(value: 1))
+            }
+        }
+    }
+}
