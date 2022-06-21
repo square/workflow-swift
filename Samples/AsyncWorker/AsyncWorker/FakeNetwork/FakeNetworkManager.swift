cc065cf3bbd0357e8394e3a97d776439bbdd8dee
Mark Johnson <markj@squareup.com>
2022-06-21 15:27:32 -0400

    Added AsyncWorker sample app
    Added app that demostrates using Workflow concurrency Worker.

---
+++ Samples/AsyncWorker/AsyncWorker/FakeNetwork/FakeNetworkManager.swift
@@ -0,0 +1,37 @@
+//
+//  FakeNetworkManager.swift
+//  AsyncWorker
+//
+//  Created by Mark Johnson on 6/16/22.
+//
+
+import Foundation
+
+class FakeNetworkManager {
+    static func makeFakeNetworkRequest() -> FakeRequest {
+        return FakeRequest()
+    }
+}
+
+class FakeRequest {
+    enum FakeRequestError: Error {
+        case cancelled
+    }
+
+    var cancelled: Bool = false
+
+    func cancel() {
+        cancelled = true
+    }
+
+    func perform(completion: @escaping (Result<Model, Error>) -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1)) {
+            guard !self.cancelled else {
+                completion(.failure(FakeRequestError.cancelled))
+                return
+            }
+
+            completion(.success(Model(message: "Request Successful!")))
+        }
+    }
+}
