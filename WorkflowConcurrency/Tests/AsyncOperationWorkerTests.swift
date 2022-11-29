c069c3acf2829e917ef0d36e6c35a7eadf958e55
Mark Johnson <markj@squareup.com>
2022-11-29 12:22:01 -0500

    Added unit test for AsyncOperationWorker

---
+++ WorkflowConcurrency/Tests/AsyncOperationWorkerTests.swift
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2022 Square Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Workflow
+import WorkflowTesting
+import XCTest
+@testable import WorkflowConcurrency
+
+@available(iOS 13.0, macOS 10.15, *)
+final class AsyncOperationWorkerTests: XCTestCase {
+    func testWorkerOutput() {
+        let host = WorkflowHost(
+            workflow: TestAsyncOperationWorkerWorkflow(key: "")
+        )
+
+        let expectation = XCTestExpectation()
+        let disposable = host.rendering.signal.observeValues { rendering in
+            expectation.fulfill()
+        }
+
+        XCTAssertEqual(0, host.rendering.value)
+
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertEqual(1, host.rendering.value)
+
+        disposable?.dispose()
+    }
+
+    func testCancelAsyncOperationWorker() {
+        struct WorkerWorkflow: Workflow {
+            typealias State = Void
+
+            enum Mode {
+                case notWorking
+                case working(start: XCTestExpectation, end: XCTestExpectation)
+            }
+
+            let mode: Mode
+
+            func render(state: State, context: RenderContext<WorkerWorkflow>) -> Bool {
+                switch mode {
+                case .notWorking:
+                    return false
+                case .working(start: let startExpectation, end: let endExpectation):
+                    AsyncOperationWorker {
+                        await asyncOperation(startExpectation: startExpectation, endExpectation: endExpectation)
+                    }
+                    .mapOutput { _ in AnyWorkflowAction.noAction }
+                    .running(in: context)
+                    return true
+                }
+            }
+
+            func asyncOperation(startExpectation: XCTestExpectation, endExpectation: XCTestExpectation) async -> Void {
+                startExpectation.fulfill()
+                for _ in 1 ... 200 {
+                    if Task.isCancelled {
+                        endExpectation.fulfill()
+                        return
+                    }
+                    try? await Task.sleep(nanoseconds: 10000000)
+                }
+                endExpectation.fulfill()
+            }
+        }
+
+        let startExpectation = XCTestExpectation()
+        let endExpectation = XCTestExpectation()
+        let host = WorkflowHost(
+            workflow: WorkerWorkflow(mode: .working(
+                start: startExpectation,
+                end: endExpectation
+            ))
+        )
+
+        wait(for: [startExpectation], timeout: 1.0)
+
+        host.update(workflow: WorkerWorkflow(mode: .notWorking))
+
+        wait(for: [endExpectation], timeout: 1.0)
+    }
+}
+
+@available(iOS 13.0, macOS 10.15, *)
+private struct TestAsyncOperationWorkerWorkflow: Workflow {
+    typealias State = Int
+    typealias Rendering = Int
+
+    let key: String
+
+    func makeInitialState() -> Int { 0 }
+
+    func render(state: Int, context: RenderContext<TestAsyncOperationWorkerWorkflow>) -> Int {
+        AsyncOperationWorker(outputOne)
+            .mapOutput { output in
+                AnyWorkflowAction { state in
+                    state = output
+                    return nil
+                }
+            }
+            .running(in: context, key: key)
+        return state
+    }
+
+    func outputOne() async -> Int {
+        return 1
+    }
+}
