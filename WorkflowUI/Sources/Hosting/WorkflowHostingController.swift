d78c0ecec5c70f1128ffeb0dfc1169e7bf308d86
jamieQ <jquadri@squareup.com>
2023-02-06 16:45:48 -0600

    anticipate merge conflicts

--- WorkflowUI/Sources/Hosting/WorkflowHostingController.swift
+++ WorkflowUI/Sources/Hosting/WorkflowHostingController.swift
@@ -16,163 +16,163 @@
 
 #if canImport(UIKit)
 
-    import ReactiveSwift
-    import UIKit
-    import Workflow
+import ReactiveSwift
+import UIKit
+import Workflow
 
-    /// Drives view controllers from a root Workflow.
-    public final class WorkflowHostingController<ScreenType, Output>: UIViewController where ScreenType: Screen {
-        /// Emits output events from the bound workflow.
-        public var output: Signal<Output, Never> {
-            return workflowHost.output
+/// Drives view controllers from a root Workflow.
+public final class WorkflowHostingController<ScreenType, Output>: UIViewController where ScreenType: Screen {
+    /// Emits output events from the bound workflow.
+    public var output: Signal<Output, Never> {
+        return workflowHost.output
+    }
+
+    private(set) var rootViewController: UIViewController
+
+    private let workflowHost: WorkflowHost<RootWorkflow<ScreenType, Output>>
+
+    private let (lifetime, token) = Lifetime.make()
+
+    public var rootViewEnvironment: ViewEnvironment {
+        didSet {
+            update(screen: workflowHost.rendering.value, environment: rootViewEnvironment)
         }
+    }
 
-        private(set) var rootViewController: UIViewController
+    public init<W: AnyWorkflowConvertible>(
+        workflow: W,
+        rootViewEnvironment: ViewEnvironment = .empty,
+        observers: [WorkflowObserver] = []
+    ) where W.Rendering == ScreenType, W.Output == Output {
+        self.workflowHost = WorkflowHost(
+            workflow: RootWorkflow(workflow),
+            observers: observers
+        )
 
-        private let workflowHost: WorkflowHost<RootWorkflow<ScreenType, Output>>
+        self.rootViewController = workflowHost
+            .rendering
+            .value
+            .buildViewController(in: rootViewEnvironment)
 
-        private let (lifetime, token) = Lifetime.make()
+        self.rootViewEnvironment = rootViewEnvironment
 
-        public var rootViewEnvironment: ViewEnvironment {
-            didSet {
-                update(screen: workflowHost.rendering.value, environment: rootViewEnvironment)
+        super.init(nibName: nil, bundle: nil)
+
+        addChild(rootViewController)
+        rootViewController.didMove(toParent: self)
+
+        workflowHost
+            .rendering
+            .signal
+            .take(during: lifetime)
+            .observeValues { [weak self] screen in
+                guard let self = self else { return }
+
+                self.update(screen: screen, environment: self.rootViewEnvironment)
             }
-        }
-
-        public init<W: AnyWorkflowConvertible>(
-            workflow: W,
-            rootViewEnvironment: ViewEnvironment = .empty,
-            observers: [WorkflowObserver] = []
-        ) where W.Rendering == ScreenType, W.Output == Output {
-            self.workflowHost = WorkflowHost(
-                workflow: RootWorkflow(workflow),
-                observers: observers
-            )
-
-            self.rootViewController = workflowHost
-                .rendering
-                .value
-                .buildViewController(in: rootViewEnvironment)
-
-            self.rootViewEnvironment = rootViewEnvironment
-
-            super.init(nibName: nil, bundle: nil)
-
-            addChild(rootViewController)
-            rootViewController.didMove(toParent: self)
-
-            workflowHost
-                .rendering
-                .signal
-                .take(during: lifetime)
-                .observeValues { [weak self] screen in
-                    guard let self = self else { return }
-
-                    self.update(screen: screen, environment: self.rootViewEnvironment)
-                }
-        }
-
-        /// Updates the root Workflow in this container.
-        public func update<W: AnyWorkflowConvertible>(workflow: W) where W.Rendering == ScreenType, W.Output == Output {
-            workflowHost.update(workflow: RootWorkflow(workflow))
-        }
-
-        public required init?(coder aDecoder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-
-        private func update(screen: ScreenType, environment: ViewEnvironment) {
-            update(child: \.rootViewController, with: screen, in: environment)
-
-            updatePreferredContentSizeIfNeeded()
-        }
-
-        override public func viewDidLoad() {
-            super.viewDidLoad()
-
-            view.backgroundColor = .white
-
-            rootViewController.view.frame = view.bounds
-            view.addSubview(rootViewController.view)
-
-            updatePreferredContentSizeIfNeeded()
-        }
-
-        override public func viewDidLayoutSubviews() {
-            super.viewDidLayoutSubviews()
-            rootViewController.view.frame = view.bounds
-        }
-
-        override public var childForStatusBarStyle: UIViewController? {
-            return rootViewController
-        }
-
-        override public var childForStatusBarHidden: UIViewController? {
-            return rootViewController
-        }
-
-        override public var childForHomeIndicatorAutoHidden: UIViewController? {
-            return rootViewController
-        }
-
-        override public var childForScreenEdgesDeferringSystemGestures: UIViewController? {
-            return rootViewController
-        }
-
-        override public var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-            return rootViewController.supportedInterfaceOrientations
-        }
-
-        override public var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
-            return rootViewController.preferredStatusBarUpdateAnimation
-        }
-
-        @available(iOS 14.0, *)
-        override public var childViewControllerForPointerLock: UIViewController? {
-            return rootViewController
-        }
-
-        override public func preferredContentSizeDidChange(
-            forChildContentContainer container: UIContentContainer
-        ) {
-            super.preferredContentSizeDidChange(forChildContentContainer: container)
-
-            guard container === rootViewController else { return }
-
-            updatePreferredContentSizeIfNeeded()
-        }
-
-        private func updatePreferredContentSizeIfNeeded() {
-            let newPreferredContentSize = rootViewController.preferredContentSize
-
-            guard newPreferredContentSize != preferredContentSize else { return }
-
-            preferredContentSize = newPreferredContentSize
-        }
     }
 
-    /// Wrapper around an AnyWorkflow that allows us to have a concrete
-    /// WorkflowHost without WorkflowHostingController itself being generic
-    /// around a Workflow.
-    fileprivate struct RootWorkflow<Rendering, Output>: Workflow {
-        typealias State = Void
-        typealias Output = Output
-        typealias Rendering = Rendering
-
-        var wrapped: AnyWorkflow<Rendering, Output>
-
-        init<W: AnyWorkflowConvertible>(_ wrapped: W) where W.Rendering == Rendering, W.Output == Output {
-            self.wrapped = wrapped.asAnyWorkflow()
-        }
-
-        func render(state: State, context: RenderContext<RootWorkflow>) -> Rendering {
-            return wrapped
-                .mapOutput { AnyWorkflowAction(sendingOutput: $0) }
-                .rendered(in: context)
-        }
+    /// Updates the root Workflow in this container.
+    public func update<W: AnyWorkflowConvertible>(workflow: W) where W.Rendering == ScreenType, W.Output == Output {
+        workflowHost.update(workflow: RootWorkflow(workflow))
     }
 
-    @available(*, deprecated, renamed: "WorkflowHostingController")
-    public typealias ContainerViewController = WorkflowHostingController
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func update(screen: ScreenType, environment: ViewEnvironment) {
+        update(child: \.rootViewController, with: screen, in: environment)
+
+        updatePreferredContentSizeIfNeeded()
+    }
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+
+        rootViewController.view.frame = view.bounds
+        view.addSubview(rootViewController.view)
+
+        updatePreferredContentSizeIfNeeded()
+    }
+
+    override public func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        rootViewController.view.frame = view.bounds
+    }
+
+    override public var childForStatusBarStyle: UIViewController? {
+        return rootViewController
+    }
+
+    override public var childForStatusBarHidden: UIViewController? {
+        return rootViewController
+    }
+
+    override public var childForHomeIndicatorAutoHidden: UIViewController? {
+        return rootViewController
+    }
+
+    override public var childForScreenEdgesDeferringSystemGestures: UIViewController? {
+        return rootViewController
+    }
+
+    override public var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return rootViewController.supportedInterfaceOrientations
+    }
+
+    override public var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+        return rootViewController.preferredStatusBarUpdateAnimation
+    }
+
+    @available(iOS 14.0, *)
+    override public var childViewControllerForPointerLock: UIViewController? {
+        return rootViewController
+    }
+
+    override public func preferredContentSizeDidChange(
+        forChildContentContainer container: UIContentContainer
+    ) {
+        super.preferredContentSizeDidChange(forChildContentContainer: container)
+
+        guard container === rootViewController else { return }
+
+        updatePreferredContentSizeIfNeeded()
+    }
+
+    private func updatePreferredContentSizeIfNeeded() {
+        let newPreferredContentSize = rootViewController.preferredContentSize
+
+        guard newPreferredContentSize != preferredContentSize else { return }
+
+        preferredContentSize = newPreferredContentSize
+    }
+}
+
+/// Wrapper around an AnyWorkflow that allows us to have a concrete
+/// WorkflowHost without WorkflowHostingController itself being generic
+/// around a Workflow.
+fileprivate struct RootWorkflow<Rendering, Output>: Workflow {
+    typealias State = Void
+    typealias Output = Output
+    typealias Rendering = Rendering
+
+    var wrapped: AnyWorkflow<Rendering, Output>
+
+    init<W: AnyWorkflowConvertible>(_ wrapped: W) where W.Rendering == Rendering, W.Output == Output {
+        self.wrapped = wrapped.asAnyWorkflow()
+    }
+
+    func render(state: State, context: RenderContext<RootWorkflow>) -> Rendering {
+        return wrapped
+            .mapOutput { AnyWorkflowAction(sendingOutput: $0) }
+            .rendered(in: context)
+    }
+}
+
+@available(*, deprecated, renamed: "WorkflowHostingController")
+public typealias ContainerViewController = WorkflowHostingController
 
 #endif
