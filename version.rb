7333a212d6638397cd57c620b85c9468953ab8ad
jamieQ <jquadri@squareup.com>
2023-05-22 15:49:24 -0500

    [release]: bump version to 2.5.0 (#217)

--- version.rb
+++ version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # The primary version number for Workflow-related pods
-WORKFLOW_VERSION ||= '2.4.0'
+WORKFLOW_VERSION ||= '2.5.0'
 
 # iOS deployment target
 WORKFLOW_IOS_DEPLOYMENT_TARGET ||= '14.0'
