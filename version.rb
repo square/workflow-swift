f7cb36c0f0d151f48cbf5c8398f66a42ae1f22a1
jamieQ <jquadri@squareup.com>
2024-01-11 11:36:04 -0600

    [chore]: make version numbers more granular

--- version.rb
+++ version.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
-# The primary version number for Workflow-related pods
-WORKFLOW_VERSION ||= '3.6.0'
+# The primary version numbers for Workflow-related pods
+WORKFLOW_MAJOR_VERSION ||= '3'
+WORKFLOW_MINOR_VERSION ||= '6'
+WORKFLOW_PATCH_VERSION ||= '0'
+WORKFLOW_VERSION ||= "#{WORKFLOW_MAJOR_VERSION}.#{WORKFLOW_MINOR_VERSION}.#{WORKFLOW_PATCH_VERSION}"
 
 # iOS deployment target
 WORKFLOW_IOS_DEPLOYMENT_TARGET ||= '14.0'
