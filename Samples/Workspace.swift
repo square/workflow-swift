951316dd425e1728be4d226b19eaa80f8d4887ce
Andrew Watt <awatt@squareup.com>
2024-09-06 16:37:56 -0700

    update ci config for tuist project

--- Samples/Workspace.swift
+++ Samples/Workspace.swift
@@ -1,6 +1,36 @@
 import ProjectDescription
+import ProjectDescriptionHelpers
 
 let workspace = Workspace(
     name: "Development",
-    projects: [".", "Tutorial"]
+    projects: [".", "Tutorial"],
+    schemes: [
+        // Generate a scheme for each target in Package.swift for convenience
+        .workflow("Workflow"),
+        .workflow("WorkflowTesting"),
+        .workflow("WorkflowUI"),
+        .workflow("WorkflowSwiftUI"),
+        .workflow("WorkflowSwiftUIMacros"),
+        .workflow("WorkflowReactiveSwift"),
+        .workflow("WorkflowReactiveSwiftTesting"),
+        .workflow("WorkflowRxSwift"),
+        .workflow("WorkflowRxSwiftTesting"),
+        .workflow("WorkflowCombine"),
+        .workflow("WorkflowCombineTesting"),
+        .workflow("WorkflowConcurrency"),
+        .workflow("WorkflowConcurrencyTesting"),
+        .workflow("ViewEnvironment"),
+        .workflow("ViewEnvironmentUI"),
+        .workflow("WorkflowSwiftUIExperimental"),
+    ]
 )
+
+
+extension Scheme {
+    public static func workflow(_ target: String) -> Self {
+        .scheme(
+            name: target,
+            buildAction: .buildAction(targets: [.project(path: "..", target: target)])
+        )
+    }
+}
