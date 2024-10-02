9b4728bbf86d2e5352ecf81145e2f288c6b56bdd
Andrew Watt <awatt@squareup.com>
2024-10-01 18:20:34 -0700

    build: enforce explicit deps in tuist

---
+++ Samples/Tuist/Config.swift
@@ -0,0 +1,8 @@
+import ProjectDescription
+
+let config = Config(
+// This breaks snapshot tests, because iOSSnapshotTestCase depends on XCTest.
+// ENABLE_TESTING_SEARCH_PATHS should sufficient but doesn't seem to work with
+// enforceExplicitDependencies enabled.
+//    generationOptions: .options(enforceExplicitDependencies: true)
+)
