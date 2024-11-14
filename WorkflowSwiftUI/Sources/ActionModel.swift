87c8f25fc618313b7a5b93fe33202c827f8d2e5d
Andrew Watt <100192+watt@users.noreply.github.com>
2024-11-14 12:02:00 -0800

    feat: expose helpers for static models (#311)
    Creating a static model is useful for testing, and the missing `public init` on `ActionModel` makes that difficult to do. This PR adds that init along with a couple static factory methods for that use case.

--- WorkflowSwiftUI/Sources/ActionModel.swift
+++ WorkflowSwiftUI/Sources/ActionModel.swift
@@ -1,10 +1,22 @@
 /// An ``ObservableModel`` for workflows with a single action.
 ///
-/// Rather than creating this model directly, you should use the
-/// ``Workflow/RenderContext/makeActionModel(state:)`` method to create an instance of this model.
+/// To create an accessor, use
+/// ``Workflow/RenderContext/makeActionModel(state:)``. State writes and actions
+/// will be sent to the workflow.
 public struct ActionModel<State: ObservableState, Action>: ObservableModel, SingleActionModel {
     public let accessor: StateAccessor<State>
     public let sendAction: (Action) -> Void
+
+    /// Creates a new ActionModel.
+    ///
+    /// Rather than creating this model directly, you should usually use the
+    /// ``Workflow/RenderContext/makeActionModel(state:)`` method to create an
+    /// instance of this model. If you need a static model for testing or
+    /// previews, you can use the ``constant(state:)`` method.
+    public init(accessor: StateAccessor<State>, sendAction: @escaping (Action) -> Void) {
+        self.accessor = accessor
+        self.sendAction = sendAction
+    }
 }
 
 /// An observable model with a single action.
@@ -22,3 +34,15 @@ extension ActionModel: Identifiable where State: Identifiable {
         accessor.id
     }
 }
+
+#if DEBUG
+
+public extension ActionModel {
+    /// Creates a static model which ignores all sent values, suitable for static previews
+    /// or testing.
+    static func constant(state: State) -> ActionModel<State, Action> {
+        ActionModel(accessor: .constant(state: state), sendAction: { _ in })
+    }
+}
+
+#endif
