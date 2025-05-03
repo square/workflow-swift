898e3f0a1d8b8ce198fa9c9fd12756aef7d28417
Andrew Watt <awatt@squareup.com>
2025-05-02 19:03:08 -0700

    feat: ObservableScreen view controller preferences

--- WorkflowSwiftUI/Tests/ObservableScreenTests.swift
+++ WorkflowSwiftUI/Tests/ObservableScreenTests.swift
@@ -11,10 +11,43 @@ final class ObservableScreenTests: XCTestCase {
         // Ensure that environment customizations made on the view controller
         // are propagated to the SwiftUI view environment.
 
-        var state = MyState()
+        struct KeyCapturingModel: ObservableModel {
+            typealias State = KeyCapturingState
+
+            let accessor: StateAccessor<State>
+        }
+
+        struct TestKeyEmittingScreen: ObservableScreen {
+            typealias Model = KeyCapturingModel
+
+            var model: Model
+
+            let sizingOptions: WorkflowSwiftUI.SwiftUIScreenSizingOptions = [.preferredContentSize]
+
+            static func makeView(store: Store<Model>) -> some View {
+                ContentView(store: store)
+            }
+
+            struct ContentView: View {
+                @Environment(\.viewEnvironment.testKey)
+                var testValue: Int
+
+                var store: Store<Model>
+
+                var body: some View {
+                    WithPerceptionTracking {
+                        let _ = { store.emittedValue = testValue }()
+                        Color.clear
+                            .frame(width: 1, height: 1)
+                    }
+                }
+            }
+        }
+
+        var state = KeyCapturingState()
 
         let viewController = TestKeyEmittingScreen(
-            model: MyModel(
+            model: KeyCapturingModel(
                 accessor: StateAccessor(
                     state: state,
                     sendValue: { $0(&state) }
@@ -33,6 +66,125 @@ final class ObservableScreenTests: XCTestCase {
 
         withExtendedLifetime(lifetime) {}
     }
+
+    func test_viewControllerPreferences() {
+        typealias Model = StateAccessor<DummyState>
+
+        let statusBarStyleQueried = expectation(description: "statusBarStyleQueried")
+        let prefersStatusBarHiddenQueried = expectation(description: "prefersStatusBarHiddenQueried")
+        let preferredStatusBarUpdateAnimationQueried = expectation(description: "preferredStatusBarUpdateAnimationQueried")
+        let supportedInterfaceOrientationsQueried = expectation(description: "supportedInterfaceOrientationsQueried")
+        let preferredScreenEdgesDeferringSystemGesturesQueried = expectation(description: "preferredScreenEdgesDeferringSystemGesturesQueried")
+        let prefersHomeIndicatorAutoHiddenQueried = expectation(description: "prefersHomeIndicatorAutoHiddenQueried")
+        let pressesBeganQueried = expectation(description: "pressesBeganQueried")
+        let accessibilityPerformEscapeQueried = expectation(description: "accessibilityPerformEscapeQueried")
+
+        struct PrefScreen: ObservableScreen {
+            let _statusBarStyle = UIStatusBarStyle.lightContent
+            let _prefersStatusBarHidden = true
+            let _preferredStatusBarUpdateAnimation = UIStatusBarAnimation.slide
+            let _supportedInterfaceOrientations: UIInterfaceOrientationMask = .all
+            let _preferredScreenEdgesDeferringSystemGestures: UIRectEdge = .top
+            let _prefersHomeIndicatorAutoHidden = true
+            let _pressesBegan = true
+            let _accessibilityPerformEscape = true
+
+            let statusBarStyleQueried: XCTestExpectation
+            let prefersStatusBarHiddenQueried: XCTestExpectation
+            let preferredStatusBarUpdateAnimationQueried: XCTestExpectation
+            let supportedInterfaceOrientationsQueried: XCTestExpectation
+            let preferredScreenEdgesDeferringSystemGesturesQueried: XCTestExpectation
+            let prefersHomeIndicatorAutoHiddenQueried: XCTestExpectation
+            let pressesBeganQueried: XCTestExpectation
+            let accessibilityPerformEscapeQueried: XCTestExpectation
+
+            let model: Model
+            static func makeView(store: Store<Model>) -> some View { EmptyView() }
+
+            public func preferredStatusBarStyle(in context: ObservableScreenContext) -> UIStatusBarStyle {
+                statusBarStyleQueried.fulfill()
+                return _statusBarStyle
+            }
+
+            public func prefersStatusBarHidden(in context: ObservableScreenContext) -> Bool {
+                prefersStatusBarHiddenQueried.fulfill()
+                return _prefersStatusBarHidden
+            }
+
+            public func preferredStatusBarUpdateAnimation(
+                in context: ObservableScreenContext
+            ) -> UIStatusBarAnimation {
+                preferredStatusBarUpdateAnimationQueried.fulfill()
+                return _preferredStatusBarUpdateAnimation
+            }
+
+            public func supportedInterfaceOrientations(
+                in context: ObservableScreenContext
+            ) -> UIInterfaceOrientationMask {
+                supportedInterfaceOrientationsQueried.fulfill()
+                return _supportedInterfaceOrientations
+            }
+
+            public func preferredScreenEdgesDeferringSystemGestures(
+                in context: ObservableScreenContext
+            ) -> UIRectEdge {
+                preferredScreenEdgesDeferringSystemGesturesQueried.fulfill()
+                return _preferredScreenEdgesDeferringSystemGestures
+            }
+
+            public func prefersHomeIndicatorAutoHidden(in context: ObservableScreenContext) -> Bool {
+                prefersHomeIndicatorAutoHiddenQueried.fulfill()
+                return _prefersHomeIndicatorAutoHidden
+            }
+
+            public func pressesBegan(_ presses: Set<UIPress>, with event: UIPressesEvent?) -> Bool {
+                pressesBeganQueried.fulfill()
+                return _pressesBegan
+            }
+
+            public func accessibilityPerformEscape() -> Bool {
+                accessibilityPerformEscapeQueried.fulfill()
+                return _accessibilityPerformEscape
+            }
+        }
+
+        let screen = PrefScreen(
+            statusBarStyleQueried: statusBarStyleQueried,
+            prefersStatusBarHiddenQueried: prefersStatusBarHiddenQueried,
+            preferredStatusBarUpdateAnimationQueried: preferredStatusBarUpdateAnimationQueried,
+            supportedInterfaceOrientationsQueried: supportedInterfaceOrientationsQueried,
+            preferredScreenEdgesDeferringSystemGesturesQueried: preferredScreenEdgesDeferringSystemGesturesQueried,
+            prefersHomeIndicatorAutoHiddenQueried: prefersHomeIndicatorAutoHiddenQueried,
+            pressesBeganQueried: pressesBeganQueried,
+            accessibilityPerformEscapeQueried: accessibilityPerformEscapeQueried,
+            model: Model.constant(state: DummyState())
+        )
+
+        let viewController = screen.buildViewController(in: .empty)
+
+        XCTAssertEqual(viewController.preferredStatusBarStyle, screen._statusBarStyle)
+        XCTAssertEqual(viewController.prefersStatusBarHidden, screen._prefersStatusBarHidden)
+        XCTAssertEqual(viewController.preferredStatusBarUpdateAnimation, screen._preferredStatusBarUpdateAnimation)
+        XCTAssertEqual(viewController.supportedInterfaceOrientations, screen._supportedInterfaceOrientations)
+        XCTAssertEqual(viewController.preferredScreenEdgesDeferringSystemGestures, screen._preferredScreenEdgesDeferringSystemGestures)
+        XCTAssertEqual(viewController.prefersHomeIndicatorAutoHidden, screen._prefersHomeIndicatorAutoHidden)
+        viewController.pressesBegan([], with: nil)
+        XCTAssertEqual(viewController.accessibilityPerformEscape(), screen._accessibilityPerformEscape)
+
+        wait(
+            for: [
+                statusBarStyleQueried,
+                prefersStatusBarHiddenQueried,
+                preferredStatusBarUpdateAnimationQueried,
+                supportedInterfaceOrientationsQueried,
+                preferredScreenEdgesDeferringSystemGesturesQueried,
+                prefersHomeIndicatorAutoHiddenQueried,
+                pressesBeganQueried,
+                accessibilityPerformEscapeQueried,
+            ],
+            timeout: 0
+        )
+    }
 }
 
 private struct TestKey: ViewEnvironmentKey {
@@ -47,41 +199,11 @@ extension ViewEnvironment {
 }
 
 @ObservableState
-private struct MyState {
+private struct KeyCapturingState {
     var emittedValue: TestKey.Value?
 }
 
-private struct MyModel: ObservableModel {
-    typealias State = MyState
-
-    let accessor: StateAccessor<State>
-}
-
-private struct TestKeyEmittingScreen: ObservableScreen {
-    typealias Model = MyModel
-
-    var model: Model
-
-    let sizingOptions: WorkflowSwiftUI.SwiftUIScreenSizingOptions = [.preferredContentSize]
-
-    static func makeView(store: Store<Model>) -> some View {
-        ContentView(store: store)
-    }
-
-    struct ContentView: View {
-        @Environment(\.viewEnvironment.testKey)
-        var testValue: Int
-
-        var store: Store<Model>
-
-        var body: some View {
-            WithPerceptionTracking {
-                let _ = { store.emittedValue = testValue }()
-                Color.clear
-                    .frame(width: 1, height: 1)
-            }
-        }
-    }
-}
+@ObservableState
+private struct DummyState {}
 
 #endif
