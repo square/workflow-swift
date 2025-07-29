dd86c46ea77b36a6d903bdec3bf92c15b7fdc228
Jamie <2119834+jamieQ@users.noreply.github.com>
2025-07-29 15:30:47 -0500

    [gardening]: remove doc comment for nonextant parameter (#363)

--- Workflow/Sources/WorkflowNode.swift
+++ Workflow/Sources/WorkflowNode.swift
@@ -121,8 +121,6 @@ final class WorkflowNode<WorkflowType: Workflow> {
 
     /// Internal method that forwards the render call through the underlying `subtreeManager`,
     /// and eventually to the client-specified `Workflow` instance.
-    /// - Parameter isRootNode: whether or not this is the root node of the tree. Note, this
-    /// is currently only used as a hint for the logging infrastructure, and is up to callers to correctly specify.
     /// - Returns: A `Rendering` of appropriate type
     func render() -> WorkflowType.Rendering {
         WorkflowLogger.logWorkflowStartedRendering(ref: self)
